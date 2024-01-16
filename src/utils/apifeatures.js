2c12491e68059ed69497fb84237b414ae03a4a1a
codecraft26 <gaman0221@gmial.com>
2024-01-17 00:37:43 +0530

    Add getAllTickets route and ApiFeatures class for pagination and sorting filter

---
+++ src/utils/apifeatures.js
@@ -0,0 +1,42 @@
+class ApiFeatures {
+    constructor(query, queryStr) {
+      this.query = query;
+      this.queryStr = queryStr;
+    }
+
+
+    //create a filter on the basis of query string of status,assignedTo,severity,type
+    filter(){
+        const queryCopy={...this.queryStr};
+        //removing fields from the query
+        const removeFields=['sort','page','limit'];
+        removeFields.forEach(el=>delete queryCopy[el]);
+        //advance filter for price, ratings etc
+        let queryStr=JSON.stringify(queryCopy);
+        queryStr=queryStr.replace(/\b(gt|gte|lt|lte)\b/g,match=>`$${match}`);
+        this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+  
+
+    sort() {
+        if (this.queryStr.sort) {
+            const sortBy = this.queryStr.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            // Default sorting
+            this.query = this.query.sort('-dateCreated');
+        }
+        return this;
+    }
+    pagination(resultPerPage) {
+      const currentPage = Number(this.queryStr.page) || 1;
+      const skip = resultPerPage * (currentPage - 1);
+      this.query = this.query.limit(resultPerPage).skip(skip);
+  
+      return this;
+    }
+  }
+  
+  export default ApiFeatures;
+  
